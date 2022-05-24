baa40417d5d0d8b7980356f1d2c5b6aefc93549f
qbarnes <quinton.barnes68@gmail.com>
2022-05-24 15:20:40 -0500

    added method and class docs

--- sat-track/src/satellite.py
+++ sat-track/src/satellite.py
@@ -24,12 +24,16 @@ class SimpleSatellite(OrbitalElements):
         return self._body
 
     def setBody(self, body: Body):
-        """Sets the body the satellite orbits. This really only be used when satellites switch bodies after escaping
-        one body."""
+        """Sets the body the satellite orbits. This really only be used when satellites switch
+        bodies after escaping one body."""
         self._body = body
 
 
 class Satellite(_SGP4_Propagator):
+    """This object describes a satellite that can be modeled with an SGP type propagation model,
+    namely the SGP4 or SDP4, described by the NORAD two-line element set.
+    The class also contains an internal buffer holding the most recent state and time associated
+    with it."""
 
     def __init__(self, tle: TwoLineElement):
         super().__init__(tle)
@@ -38,28 +42,41 @@ class Satellite(_SGP4_Propagator):
         self._recent_state = (None, None)
 
     def getState(self, jd: JulianDate):
+        """Computes the state vectors for a satellite at a given time. The Satellite object should
+        be loaded with an up-to-date TLE as the accuracy becomes less the farther from the TLE
+        epoch the desired time is.
+        Parameters:
+        jd: The time to find the state vectors of the satellite.
+        Returns a tuple with the position and velocity vectors as the first and second elements."""
         self._recent_time = jd
         state = super().getState(jd)
         self._recent_state = state
         return state
 
     def name(self) -> str:
+        """Returns the name of the satellite, which is determined by the name in the TLE."""
         return self._name
 
     def tle(self) -> TwoLineElement:
+        """Returns the most recent TLE loaded into the satellite. If the TLE hasn't been updated
+        by using updateTle(), this is the TLE used when instantiating the Satellite object."""
         return self._tle
 
     def epoch(self) -> JulianDate:
+        """The epoch found in the most recently loaded TLE."""
         return self._tleEpoch
 
     def updateTle(self, tle: TwoLineElement) -> None:
+        """Updates the TLE to a more up-to-date TLE than the one loaded when instantiating."""
         self._name = tle.getName()
         self._tle = tle
         self._tleEpoch = tle.epoch()
         self.initialize(tle)
 
     def recentTime(self) -> JulianDate:
+        """The time the most recent state was calculated."""
         return self._recent_time
 
     def recentState(self):
+        """The most recent state calculated."""
         return self._recent_state
