788f0c68cf985d2a27ba6a648b316a0972942698
qbarnes <quinton.barnes68@gmail.com>
2022-06-25 00:03:37 -0500

    moved functions from satellite into better modules

--- sat-track/src/sattrack/position.py
+++ sat-track/src/sattrack/position.py
@@ -1,14 +1,16 @@
-from math import radians, cos, sin, atan, sqrt, degrees
+from math import radians, cos, sin, atan, sqrt, degrees, atan2 as matan2
 
 from pyevspace import EVector, vang, norm, cross
 
 from sattrack.spacetime.juliandate import JulianDate
+from sattrack.spacetime.sidereal import earthOffsetAngle
+from sattrack.structures.coordinates import GeoPosition, geocentricToGeodetic
 from sattrack.structures.elements import computeEccentricVector, OrbitalElements
 
 
 from sattrack.util.anomalies import meanToTrue
 from sattrack.util.constants import EARTH_MU
-from sattrack.util.conversions import smaToMeanMotion
+from sattrack.util.conversions import smaToMeanMotion, atan2 as catan2
 
 
 def timeToMeanAnomaly(elements: OrbitalElements, meanAnom: float) -> float:
@@ -47,3 +49,12 @@ def computeFlightAngle(elements: OrbitalElements) -> float:
 def computeVelocity(elements: OrbitalElements) -> float:
     radius = computeRadius(elements)
     return sqrt(EARTH_MU * ((2 / radius) - (1 / elements.getSma())))
+
+
+def getSubPoint(position: EVector, velocity: EVector, jd: JulianDate) -> GeoPosition:
+    xyMag = sqrt(position[0]*position[0] + position[1]*position[1])
+    dec = degrees(matan2(position[2], xyMag))
+    lng = (degrees(catan2(position[1], position[0])) - earthOffsetAngle(jd)) % 360.0
+    if lng > 180.0:
+        lng = lng - 360.0
+    return GeoPosition(geocentricToGeodetic(dec), lng)
--- sat-track/src/sattrack/topos.py
+++ sat-track/src/sattrack/topos.py
@@ -1,3 +1,6 @@
+from cmath import asin
+from math import degrees
+
 from pyevspace import EVector, cross, dot, norm
 
 from sattrack.structures.coordinates import GeoPosition, geoPositionVector, zenithVector
@@ -5,6 +8,8 @@ from sattrack.rotation.order import Order
 from sattrack.rotation.rotation import getEulerMatrix, EulerAngles, rotateToThenOffset
 from sattrack.spacetime.juliandate import JulianDate
 from sattrack.spacetime.sidereal import  earthOffsetAngle
+from sattrack.structures.satellite import Satellite
+from sattrack.util.conversions import atan2
 
 
 def ijkToSEZ(vec: EVector, jd: JulianDate, geo: GeoPosition) -> EVector:
@@ -31,3 +36,21 @@ def getPVector(geo: GeoPosition, state: tuple[EVector], jd: JulianDate) -> EVect
     v = cross(zeta, lamb)
     t = dot(r, (gamma - r)) / dot(r, r)
     return r + v * t
+
+
+#   todo: moved these, not sure if i want to keep them like this
+def getToposPosition(satellite: Satellite, jd: JulianDate, geo: GeoPosition) -> EVector:
+    state = satellite.getState(jd)
+    return ijkToSEZ(state[0], jd, geo)
+
+
+def getAltitude(satellite: Satellite, jd: JulianDate, geo: GeoPosition) -> float:
+    state = satellite.getState(jd)
+    sez = ijkToSEZ(state[0], jd, geo)
+    return degrees(asin(sez[2] / sez.mag()))
+
+
+def getAzimuth(satellite: Satellite, jd: JulianDate, geo: GeoPosition) -> float:
+    state = satellite.getState(jd)
+    sez = ijkToSEZ(state[0], jd, geo)
+    return degrees(atan2(sez[1], -sez[0]))
