b860388d97737f84ef15f903dda49f6db63c1faa
qbarnes <quinton.barnes68@gmail.com>
2022-07-17 00:48:39 -0500

    documented and cleaned warnings

--- sat-track/src/sattrack/spacetime/juliandate.py
+++ sat-track/src/sattrack/spacetime/juliandate.py
@@ -2,32 +2,54 @@ from datetime import datetime
 
 
 class JulianDate:
+    """
+    Class implementation of a Julian Date, which represents a moment in time as an integer day number, and a fraction of
+    said day after 12 noon. The JulianDate can be set from the components of a Gregorian calendar date via the __init__
+    method, or to the current time using the now() module function. The future() and difference() method allow the
+    ability to perform math between different dates, or to compute future or past dates. The solar day is the unit of
+    time for these methods, which is equal to 24 hours, 1440 minutes or 86400 seconds.
+    """
 
     def __init__(self, month: int, day: int, year: int, hour: int, minute: int, second: float, timeZone: int = 0):
+        """
+        Initialize an instance from the components of a Gregorian date.
+
+        Args:
+            month: Month of the year
+            day: Day of the month
+            year: Year of the date
+            hour: Hours of the day
+            minute: Minutes of the hour
+            second: Second of the minute
+            timeZone: Timezone offset of the time (default = 0)
+        """
         self._day_number = 0
         self._day_fraction = 0
         self.setTime(month, day, year, hour, minute, second, timeZone)
 
     def __str__(self) -> str:
-        """Returns the value and Gregorian date of the JulianDate."""
+        """Returns the value and Gregorian date of the JulianDate as a string."""
         return str(round(self.value(), 6)) + ' --- ' + self.date()
 
-    def setTime(self, m: int, d: int, y: int, hr: int, mi: int, sec: float, timeZone: int = 0):
-        """Sets the value of a JulianDate object based on a Gregorian calendar date.
-        Parameters:
-        m:        Gregorian date month.
-        d:        Gregorian date day.
-        y:        Gregorian date year.
-        hr:       Gregorian date hour.
-        mi:       Gregorian date minute.
-        sec:      Gregorian date second, includes fractional portions.
-        timeZone: Timezone of the Gregorian date time, defaults to 0."""
+    def setTime(self, month: int, day: int, year: int, hour: int, minute: int, sec: float, timeZone: int = 0):
+        """
+        Sets the value of the JulianDate from the components of a Gregorian date.
 
-        t0 = int((1461 * (y + 4800 + int((m - 14) / 12))) / 4)
-        t1 = int((367 * (m - 2 - (12 * int((m - 14) / 12)))) / 12)
-        t2 = int((3 * int((y + 4900 + int((m - 14) / 12)) / 100)) / 4)
-        self._day_number = t0 + t1 - t2 + d - 32075
-        self._day_fraction = ((hr - 12) / 24.0) + (mi / 1440.0) + (sec / 86400.0) - (timeZone / 24.0)
+        Args:
+            month: Month of the year
+            day: Day of the month
+            year: Year of the date
+            hour: Hours of the day
+            minute: Minutes of the hour
+            sec: Second of the minute
+            timeZone: Timezone offset of the time (default = 0)
+        """
+
+        t0 = int((1461 * (year + 4800 + int((month - 14) / 12))) / 4)
+        t1 = int((367 * (month - 2 - (12 * int((month - 14) / 12)))) / 12)
+        t2 = int((3 * int((year + 4900 + int((month - 14) / 12)) / 100)) / 4)
+        self._day_number = t0 + t1 - t2 + day - 32075
+        self._day_fraction = ((hour - 12) / 24.0) + (minute / 1440.0) + (sec / 86400.0) - (timeZone / 24.0)
         if self._day_fraction >= 1.0:
             self._day_number += 1
             self._day_fraction -= 1.0
@@ -40,18 +62,24 @@ class JulianDate:
         return self._day_number + self._day_fraction
 
     def number(self):
-        """Returns the Julian day number of this JulianDate."""
+        """Returns the Julian day number."""
         return self._day_number
 
     def fraction(self):
-        """Returns the fractional part of this JulianDate."""
+        """Returns the fraction part of the JulianDate."""
         return self._day_fraction
 
-    def future(self, days: float):
-        """Computes a future JulianDate from this date.
-        Parameters:
-        days:   Number of solar days in the future of this date. A negative value
-                indicates a computing a past date."""
+    def future(self, days: float) -> JulianDate:
+        """
+        Returns a future or past date, relative to the calling date. A positive argument computes a future date, a
+        negative value computes a past date.
+
+        Args:
+            days: The offset between dates in solar days, where a positive value indicates forward, negative backwards.
+
+        Returns:
+            The future or past JulianDate.
+        """
 
         rtn = JulianDate(0, 0, 0, 0, 0, 0)
         int_day = int(days)
@@ -62,16 +90,33 @@ class JulianDate:
             rtn._day_fraction -= 1.0
         return rtn
 
-    def difference(self, jd) -> float:
-        """Returns the difference between two JulianDates, measured in solar days.
-        Parameters:
-        jd: A JulianDate object to find the difference from this object."""
+    def difference(self, jd: JulianDate) -> float:
+        """
+        Returns the difference in time between two JulianDates. The value is relative to the calling object, so if the
+        argument value is before the calling object, the returned value is positive. The opposite returns a negative
+        value.
+
+        Args:
+            jd: Relative JulianDate to compute the difference between.
+
+        Returns:
+            The difference between the calling date and the argument date in solar days.
+        """
+
         return self.value() - jd.value()
 
     def date(self, timezone: float = 0.0) -> str:
-        """Generates a string with the Gregorian date equivalent of this JulianDate.
-        Parameters:
-        timezone: The timezone offset to create the Gregorian date."""
+        """
+        Generates a string with the Gregorian calendar date equivalent of the calling date. The timezone offset adjusts
+        to the correct time. Printing this string allows the printed string to accommodate a timezone offset, something
+        the __str__ method can't do.
+
+        Args:
+            timezone: Timezone offset if the desired time is not UTC. (Default = 0)
+
+        Returns:
+            A string representing the date as a Gregorian calendar date.
+        """
 
         val = self.value()
         Z = int(val + 0.5 + (timezone / 24.0))
@@ -103,17 +148,29 @@ class JulianDate:
                         ))
 
     def day(self, timezone: float = 0.0) -> str:
-        """Returns a string with the day portion of the Gregorian date equivalent
-        of this JulianDate.
-        Parameters:
-        timezone: The timezone used to generate the Gregorian date, defaults to 0."""
+        """
+        Returns a string with the day portion of the Gregorian calendar date equivalent.
+
+        Args:
+            timezone: Timezone offset to adjust the day number if applicable. (Default = 0.0)
+
+        Returns:
+            A string of the day portion of the Gregorian calendar date equivalent.
+        """
+
         return self.date(timezone).split(' ')[0]
 
     def time(self, timezone: float = 0.0) -> str:
-        """Returns a string with the time portion of the Gregorian date equivalent
-        of this JulianDate.
-        Parameters:
-        timezone: The timezone used to generate the Gregorian date, defaults to 0."""
+        """
+        Returns a string with the time portion of the Gregorian calendar date equivalent.
+
+        Args:
+            timezone: Timezone offset to adjust the time. (Default = 0.0)
+
+        Returns:
+
+        """
+
         return self.date(timezone).split(' ')[1]
 
 
--- sat-track/src/sattrack/spacetime/sidereal.py
+++ sat-track/src/sattrack/spacetime/sidereal.py
@@ -1,25 +1,37 @@
 from sattrack.spacetime.juliandate import JulianDate, J2000
+from sattrack.util.constants import TWOPI
 
 
 def siderealTime(jd: JulianDate) -> float:
-    """Converts a certain solar time into sidereal time, most useful for computing Earth's
-    geographic reference frame offset from the geocentric equitorial celestial reference frame.
-    I.e. this is the offset angle of GMT to the first point of Aries measured in hours.
-    Parameters:
-    jd: The time to convert.
-    return: The local sidereal time of Greenwich, England."""
+    """
+    Converts a solar time into sidereal time, most useful for computing the right-ascension of earth's zero longitude,
+    equal to the geographic reference frame offset from the geocentric equitorial celestial reference frame.
+
+    Args:
+        jd: Solar time to convert to sidereal time.
+
+    Returns:
+        The Earth's sidereal time at the given solar time.
+    """
+
     dt = jd.difference(J2000)
     tmp = (18.697_374_558 + 24.065_709_824_419_08 * dt) % 24.0
     return tmp + 24.0 if tmp < 0 else tmp
 
 
 def localSiderealTime(jd: JulianDate, lng: float) -> float:
-    """Converts a certain solar time into local sidereal time, useful for computing your
-    longitudinal offset from the geocentric equitorial celestial reference frame.
-    Parameters:
-    jd:     The time to convert.
-    lng:    The local longitude.
-    Returns the local sidereal time in hours."""
+    """
+    Computes the sidereal time for a line of longitude on Earth, which is equivalent to the offset angle between a line
+    of longitude and the first point of Ares, or the celestial x-axis.
+
+    Args:
+        jd: Solar time to convert to sidereal time.
+        lng: Longitude of the GeoPosition whose local sidereal time is being found.
+
+    Returns:
+        The local sidereal time of a longitude at the given solar time.
+    """
+
     if lng < 0:
         lng += 360.0
     lng2RA = lng / 360.0 * 24.0
@@ -28,9 +40,5 @@ def localSiderealTime(jd: JulianDate, lng: float) -> float:
 
 
 def earthOffsetAngle(jd: JulianDate) -> float:
-    """Computes the earth offset angle from the celestial reference frame for a
-    given solar time.
-    Parameters:
-    jd: The time to compute.
-    Returns the offset angle in degrees."""
-    return siderealTime(jd) / 24.0 * 360.0
+    """Computes the earth offset angle from the celestial reference frame in radians."""
+    return siderealTime(jd) / 24.0 * TWOPI
