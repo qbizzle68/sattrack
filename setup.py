1e378edca3f8235248f15c8a681e9e987f1c9a46
qbarnes <quinton.barnes68@gmail.com>
2022-09-27 17:12:07 -0500

    merged build files from shadowless branch

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, Extension, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+ext_modules = [Extension(
+    'sattrack.sgp4',
+    include_dirs = ['extensions/sgp4/include'],
+    sources = [
+        'extensions/sgp4/src/SGP4.cpp',
+        'extensions/sgp4/src/sgp4module.cpp'
+    ]
+)]
+
+setup(
+    name = 'sattrack',
+    version = '0.0.1',
+    author = "Quinton Barnes",
+    author_email = "devqbizzle68@gmail.com",
+    description = "A satellites tracking framework.",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    license= 'MIT',
+    url = 'https://github.com/qbizzle68/sattrack',
+    install_requires=['pyevspace>=0.0.8'],
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Astronomy'
+    ],
+    packages = ['sattrack', 'sattrack.rotation', 'sattrack.spacetime', 'sattrack.structures', 'sattrack.util'],
+    package_dir = {'sattrack': 'src/sattrack'},
+    ext_modules = ext_modules,
+)
