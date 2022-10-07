53b7604f7d70a1d233d7b8ae2c6861543700ccbf
qbarnes <quinton.barnes68@gmail.com>
2022-10-06 23:36:04 -0500

    created api.py which imports everything that exists in the project

---
+++ src/sattrack/api.py
@@ -0,0 +1,16 @@
+from sattrack.rotation.order import *
+from sattrack.rotation.rotation import *
+
+from sattrack.spacetime.juliandate import *
+from sattrack.spacetime.sidereal import *
+
+from sattrack.structures.body import *
+from sattrack.structures.coordinates import *
+from sattrack.structures.elements import *
+from sattrack.structures.satellite import *
+from sattrack.structures.tle import *
+
+from sattrack.exceptions import *
+from sattrack.satpass import *
+from sattrack.sun import *
+from sattrack.topos import *
