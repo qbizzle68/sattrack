3220cd33437a4024959ee43201dd341029d08493
qbarnes <quinton.barnes68@gmail.com>
2022-10-30 16:02:43 -0500

    updated api.py

--- src/sattrack/api.py
+++ src/sattrack/api.py
@@ -4,17 +4,14 @@ from sattrack.rotation.rotation import *
 from sattrack.spacetime.juliandate import *
 from sattrack.spacetime.sidereal import *
 
-from sattrack.structures.body import *
 from sattrack.structures.coordinates import *
-from sattrack.structures.elements import *
-from sattrack.structures.satellite import *
+from sattrack.structures.orbit import *
 from sattrack.structures.tle import *
 
+from sattrack.util.constants import *
+from sattrack.util.conversions import *
+
 from sattrack.exceptions import *
-# from sattrack.satpass import *
-from sattrack.sun import *
-# from sattrack.topos import *
 from sattrack.sampa import *
 from sattrack.eclipse import *
-from sattrack.structures.orbit import *
 from sattrack.topocentric import *
