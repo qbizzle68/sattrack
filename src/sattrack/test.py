40260d51d534e1ee2be97eb0dd71bc4ecf3eeb8c
qbarnes <quinton.barnes68@gmail.com>
2022-11-04 21:53:54 -0500

    removed test.py from tracking

--- src/sattrack/test.py
+++
@@ -1,9 +0,0 @@
-from sattrack.api import *
-
-tle = getTle('zarya')
-iss = Satellite(tle)
-
-jd = now()
-geo = GeoPosition(38.0608, -97.9298)
-np = getNextPass(iss, geo, jd)
-plist = getPassList(iss, geo, jd, 2)
