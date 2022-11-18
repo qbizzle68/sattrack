de81639cbd28965254a476790f60b29acc629ad9
qbarnes <quinton.barnes68@gmail.com>
2022-11-17 19:22:22 -0600

    changed pyevspace version required

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     long_description_content_type='text',
     license='MIT',
     url='https://github.com/qbizzle68/sattrack',
-    install_requires=['pyevspace>=0.0.8', 'requests'],
+    install_requires=['pyevspace>=0.0.9', 'requests'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
