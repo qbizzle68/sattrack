6287ea914570e3a10382ad27d6d4408a6c750da6
qbarnes <quinton.barnes68@gmail.com>
2023-10-31 21:12:57 -0500

    upgraded requests usage

--- src/sattrack/orbit/tle.py
+++ src/sattrack/orbit/tle.py
@@ -1,15 +1,47 @@
+import re
+
 import requests
 
 # noinspection PyUnresolvedReferences
 from sattrack.orbit.sgp4 import TwoLineElement
 from sattrack.orbit.exceptions import NoTLEFound
 
-_CELESTRAK_URL = "https://celestrak.com/NORAD/elements/gp.php?{}={}&FORMAT=TLE"
+
+CELESTRAK_URL = "https://celestrak.com/NORAD/elements/gp.php"
 
 
-def getTle(value: str, query: str = 'name') -> TwoLineElement | None:
-    """
-    Retrieves a TLE from the Celestrak online repository of continually updating TLEs via HTTP.
+class TLEResponseIterator:
+    __slots__ = '_obj', '_n', '_length'
+
+    def __init__(self, obj: list[str]):
+        self._obj = obj
+        self._n = 0
+        self._length = len(obj)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._n >= self._length:
+            raise StopIteration
+
+        idx = self._n
+        pattern = r'[12] \d{5}[A-Z]?'
+        if not re.match(pattern, self._obj[idx]):
+            # Assume there is a name associated with the TLE.
+            inc = 3
+        else:
+            # Assume there is not a name associated with the TLE.
+            inc = 2
+
+        rtn = '\n'.join(self._obj[idx:idx + inc])
+        self._n += inc
+
+        return rtn
+
+
+def getTle(value: str, query: str = 'NAME', *, limitOne=False, interactive=False) -> TwoLineElement | None:
+    """Retrieves a TLE from the Celestrak online repository of continually updating TLEs via HTTP.
 
     Args:
         value: The value to search for, which depends on the querying type.
@@ -20,29 +52,50 @@ def getTle(value: str, query: str = 'name') -> TwoLineElement | None:
             GROUP:  Groups of satellites provided on the CelesTrak CurrentDate page.
             NAME:   Satellite Name (Default). Allows searching for satellites by parts of their name.
             SPECIAL:Special data sets for the GEO Protected Zone (GPZ) or GPZ Plus.
-    Returns:
-        A TwoLineElement object from the search parameters.
-    """
+        limitOne: Only returns a single TLE if multiple are returned (returns the first TLE in response).
+        interactive: If False and limitOne is not True, return a list of TwoLineElement object if the request returns
+            multiple TLEs. If True and limitOne is not True, asks user in put interactively to select which TLE to
+            select from the response."""
 
-    # todo: handle return codes properly
-    response = requests.get(_CELESTRAK_URL.format(query.upper(), value.replace(' ', '%20')))
-    lines = response.text.splitlines()
+    params = {query.upper(): value, "FORMAT": "TLE"}
+    response = requests.get(CELESTRAK_URL, params=params)
 
+    response.raise_for_status()
     if response.text == 'No GP data found':
-        raise NoTLEFound("No GP data found")
-    elif len(lines) == 3:
-        return TwoLineElement(response.text.strip())
+        raise NoTLEFound('No GP data found')
+
+    tleIterator = TLEResponseIterator(response.text.splitlines())
+    tleStrings = list(tleIterator)
+
+    if len(tleStrings) == 1 or limitOne:
+        return TwoLineElement(tleStrings[0])
+
+    if interactive is False:
+        # return [TwoLineElement(tle) for tle in tleStrings]
+        tmp = []
+        for tle in tleStrings:
+            print(f"'{tle}'")
+            tmp.append(TwoLineElement(tle))
+        return tmp
     else:
-        lineGroups = [(lines[i], lines[i + 1], lines[i + 2]) for i in range(0, len(lines), 3)]
-        print("Multiple TLEs found.")
-        for l, i in zip(lineGroups, range(1, len(lineGroups) + 1)):
-            print(f'{i}:  {l[0]}')
-        print(f'{len(lineGroups) + 1}:  None')
-        x = 0
-        while x not in range(1, len(lineGroups) + 2):
-            x = int(input("Which TLE do you wish to import?: "))
-        if x == len(lineGroups) + 1:
-            return None
-        i, j, k = lineGroups[x - 1]
-        tle = i + '\n' + j + '\n' + k
-        return TwoLineElement(tle)
+        return _selectTleInteractive(tleStrings)
+
+
+def _selectTleInteractive(tleStrings: list[str]) -> TwoLineElement | None:
+    idx = 0
+    tleCount = len(tleStrings)
+    while idx < 1 or idx > tleCount + 1:
+        for i, tle in enumerate(tleStrings, 1):
+            # This undoes what we did iterating over the original response, but we need the parts.
+            tleParts = tle.splitlines()
+            print(f'{i}:', end='')
+            if len(tleParts) == 3:
+                print('', tleParts[0])
+            else:
+                print('\t', '\n\t'.join(tleParts))
+        print(tleCount + 1, ': None')
+        idx = int(input('Which TLE do you wish to import?: '))
+
+    if idx == tleCount + 1:
+        return None
+    return TwoLineElement(tleStrings[idx - 1])
--- test/test_tle.py
+++ test/test_tle.py
@@ -1,11 +1,12 @@
+import contextlib
 import re
 import unittest
-from contextlib import redirect_stdout
 from io import StringIO
 from unittest import mock
 
+# noinspection PyUnresolvedReferences
 from pyevspace import Vector
-from sattrack.orbit.sgp4 import TwoLineElement
+from sattrack.orbit.tle import TwoLineElement
 
 from sattrack.orbit.exceptions import NoTLEFound
 from sattrack.orbit.satellite import Satellite
@@ -75,6 +76,9 @@ class TestTle(unittest.TestCase):
         stateList = cls.loadPositionData()
         cls.stateList = stateList
 
+        with open('test/resources/response-list.txt') as f:
+            cls.issResponse = f.read()
+
     # This skips testing sat number 26900.
     def testStates(self):
         for tle, states, (start, end, step) in zip(self.tleList, self.stateList, self.timeData):
@@ -102,6 +106,67 @@ class TestTle(unittest.TestCase):
                         exit(1)
 
     @mock.patch('sattrack.orbit.tle.requests')
+    def testNonInteractiveRequest(self, requestMock):
+        responseMock = mock.MagicMock()
+
+        # Test multiple results, default values. (limitOne=False, interactive=False)
+        responseMock.status_code = 200
+        responseMock.text = self.issResponse
+        requestMock.get.return_value = responseMock
+
+        result = getTle('iss')
+        self.assertTrue(result)
+        for tle in result:
+            with self.subTest():
+                self.assertIsInstance(tle, TwoLineElement)
+
+        # Test multiple results, limitOne=True.
+        result = getTle('iss', limitOne=True)
+        self.assertTrue(result)
+        self.assertIsInstance(result, TwoLineElement)
+
+        # Test multiple results, both options True.
+        result = getTle('iss', limitOne=True, interactive=True)
+        self.assertTrue(result)
+        self.assertIsInstance(result, TwoLineElement)
+
+        # Test interactive.
+        stream = StringIO()
+        with contextlib.redirect_stdout(stream):
+            with mock.patch('sattrack.orbit.tle.input') as mockInput:
+                mockInput.return_value = 3
+                result = getTle('iss', interactive=True)
+
+        self.assertTrue(result)
+        self.assertIsInstance(result, TwoLineElement)
+        self.assertEqual(result.name, 'ISS (ZARYA)')
+
+        # Test interactive return None.
+        with contextlib.redirect_stdout(stream):
+            with mock.patch('sattrack.orbit.tle.input') as mockInput:
+                mockInput.return_value = 16
+                result = getTle('iss', interactive=True)
+
+        self.assertIsNone(result)
+
+        # Test a 2LE response format.
+        # Mimic a 2LE format response.
+        tmp = []
+        for i, line in enumerate(self.issResponse.splitlines()):
+            if i % 3 == 1 or i % 3 == 2:
+                tmp.append(line)
+        responseText = '\n'.join(tmp)
+
+        responseMock.text = responseText
+        requestMock.get.return_value = responseMock
+
+        result = getTle('iss')
+        self.assertTrue(result)
+        for tle in result:
+            with self.subTest():
+                self.assertIsInstance(tle, TwoLineElement)
+
+    @mock.patch('sattrack.orbit.tle.requests')
     def testSingleTleRequest(self, requestMock):
         tleString = '''ISS (ZARYA)             
         1 25544U 98067A   23302.18631802  .00023916  00000+0  42890-3 0  9995
@@ -118,81 +183,16 @@ class TestTle(unittest.TestCase):
         self.assertEqual(tleResponse.line1, tle.line1)
         self.assertEqual(tleResponse.line2, tle.line2)
 
-    @mock.patch('sattrack.orbit.tle.requests')
-    @mock.patch('sattrack.orbit.tle.input')
-    def testMultipleTleRequest(self, mockInput, requestMock):
+        # Test simple return with alternate options.
+        response = getTle('zarya', limitOne=True)
+        self.assertEqual(response.name, tle.name)
+        self.assertEqual(response.line1, tle.line1)
+        self.assertEqual(response.line2, tle.line2)
 
-        # with mock.patch('sattrack.orbit.tle.requests') as requestMock:
-        tleList = '''UME (ISS)               
-1 08709U 76019A   23301.55301458  .00000133  00000+0  20482-3 0  9992
-2 08709  69.6749 342.2326 0011572 343.9923 128.3410 13.71557622385080
-UME-2 (ISS-B)           
-1 10674U 78018A   23301.67653483 -.00000035  00000+0  23054-4 0  9999
-2 10674  69.3649  32.0090 0160482  70.0218 100.9952 13.43653424241334
-ISS (ZARYA)             
-1 25544U 98067A   23302.18631802  .00023916  00000+0  42890-3 0  9995
-2 25544  51.6427  24.8067 0000539  60.5934  25.2504 15.49791535422575
-SWISSCUBE               
-1 35932U 09051B   23301.61074555  .00001721  00000+0  38408-3 0  9995
-2 35932  98.4987 175.0189 0006857 324.8711  35.2034 14.58129653748653
-AISSAT 1                
-1 36797U 10035C   23301.83449840  .00003252  00000+0  34755-3 0  9998
-2 36797  98.2114 149.6456 0010421 353.9411   6.1678 14.89489601720690
-AISSAT 2                
-1 40075U 14037G   23301.73842984  .00003397  00000+0  40316-3 0  9993
-2 40075  98.3378 204.8581 0006055 143.0406 217.1225 14.85192582503461
-ISS DEB [EP BATTERY]    
-1 47853U 98067RZ  23301.82478070  .00077059  00000+0  41673-3 0  9993
-2 47853  51.6305 333.8027 0004141  29.0261 331.0970 15.79398727149842
-ISS (NAUKA)             
-1 49044U 21066A   23300.49437812  .00020675  00000+0  37311-3 0  9999
-2 49044  51.6418  33.1919 0000639  84.5600 275.5462 15.49706576422307
-ISS DEB (SPX-26 IPA FSE)
-1 55448U 98067VB  23301.87676908  .00178538  00000+0  86823-3 0  9997
-2 55448  51.6323   1.0970 0005360 327.2528  32.8141 15.81344070 42242
-OUTPOST MISSION 1       
-1 56226U 23084BK  23301.86776365  .00014113  00000+0  73009-3 0  9996
-2 56226  97.5242  56.8569 0011452  99.3321 260.9207 15.16298168 20893
-ISS DEB [ERA OUTFITTING]
-1 56434U 98067VJ  23301.50245583  .00190397  00000+0  11507-2 0  9997
-2 56434  51.6340  15.1479 0005925 323.0615  36.9977 15.76488989 27769
-ISS DEB                 
-1 57161U 98067VL  23301.92944941  .00359164  00000+0  19119-2 0  9991
-2 57161  51.6343  15.7688 0005892 344.1544  15.9272 15.78858186 20036
-ISS DEB                 
-1 57162U 98067VM  23301.91016958  .00290812  00000+0  16954-2 0  9997
-2 57162  51.6344  16.4145 0005466 351.2702   8.8202 15.77013840 20028
-ISS DEB                 
-1 57163U 98067VN  23301.93896613  .00181225  00000+0  14429-2 0  9996
-2 57163  51.6354  18.1043 0001537 322.8791  37.2100 15.70091625 19990
-ISS DEB [SPX-28 IPA FSE]
-1 57212U 98067VP  23301.52115482  .00055723  00000+0  70334-3 0  9995
-2 57212  51.6383  24.3092 0002029 221.9658 138.1180 15.59016026 19457'''
-
-        tleString = '''ISS (ZARYA)             
-        1 25544U 98067A   23302.18631802  .00023916  00000+0  42890-3 0  9995
-        2 25544  51.6427  24.8067 0000539  60.5934  25.2504 15.49791535422575'''
-        tle = TwoLineElement(tleString)
-
-        responseMock = mock.MagicMock()
-        responseMock.status_code = 200
-        responseMock.text = tleList
-
-        requestMock.get.return_value = responseMock
-        mockInput.return_value = 3
-
-        stream = StringIO()
-        with redirect_stdout(stream):
-            tleResponse = getTle('iss')
-            self.assertEqual(tleResponse.name, tle.name)
-            self.assertEqual(tleResponse.line1, tle.line1)
-            self.assertEqual(tleResponse.line2, tle.line2)
-
-        # Select return None option.
-        mockInput.return_value = 16
-        with redirect_stdout(stream):
-            tleResponse = getTle('iss')
-            self.assertIsNone(tleResponse)
+        response = getTle('zarya', interactive=True)
+        self.assertEqual(response.name, tle.name)
+        self.assertEqual(response.line1, tle.line1)
+        self.assertEqual(response.line2, tle.line2)
 
     @mock.patch('sattrack.orbit.tle.requests')
     def testNoGPData(self, requestMock):
@@ -202,7 +202,7 @@ ISS DEB [SPX-28 IPA FSE]
 
         requestMock.get.return_value = responseMock
 
-        stream = StringIO()
-        with redirect_stdout(stream):
-            with self.assertRaises(NoTLEFound):
-                getTle('zarya2')
+        # stream = StringIO()
+        # with redirect_stdout(stream):
+        with self.assertRaises(NoTLEFound):
+            getTle('zarya2')
